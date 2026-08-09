8412a0a2f39a5eb9366d199f4b16153bada370e4
Henry Díaz Bordón <henrydiazbordon@gmail.com>
2026-08-09 14:12:45 +0100

    modificada la solución al reto 14 para usar `norm_num` en lugar de `trivial` en las relaciones numéricas

--- RetosSoftware/Reto14.lean
+++ RetosSoftware/Reto14.lean
@@ -9,10 +9,10 @@ Solución de Henry Díaz Bordón <henrydiazbordon@gmail.com>.
 
 lemma pow_2_n_plus_4_ge_2 {n : ℕ} : 2 ≤ 2^(n + 4) := by
   induction n with
-  | zero => trivial
+  | zero => norm_num
   | succ k hk =>
     calc
-      2 ≤ 2 * 2 := by trivial
+      2 ≤ 2 * 2 := by norm_num
       _ ≤ 2 * 2^(k + 4) := by rel [hk]
       _ = 2^1 * 2^(k + 4) := by ring
       _ = 2^(1 + (k + 4)) := by rw [← pow_add 2]
@@ -20,7 +20,7 @@ lemma pow_2_n_plus_4_ge_2 {n : ℕ} : 2 ≤ 2^(n + 4) := by
 
 theorem Reto14 {n : ℕ} : 2 * n + 9 ≤ 2 ^ (n + 4) := by
   induction n with
-  | zero => trivial
+  | zero => norm_num
   | succ k hk =>
     calc
       2 * (k + 1) + 9 = 2 * k + 2 + 9 := by rw [mul_add]
