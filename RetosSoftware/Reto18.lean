f4b9b9c412ba19a4efb0c73dc1ee513372182284
Henry Díaz Bordón <henrydiazbordon@gmail.com>
2026-09-09 02:27:55 +0200

    solución al reto número 18

--- RetosSoftware/Reto18.lean
+++ RetosSoftware/Reto18.lean
@@ -49,10 +49,27 @@ lemma limSuc_zero_prod_limSuc_eq_zero {a b : ℕ → ℝ} {L : ℝ}
   intro ε hε
   let ⟨N₁, hN₁⟩ := hb 1 (by norm_num)
   let B := max |L - 1| |L + 1|
-  have hB₁ : B ≠ 0 := by grind
+  -- toca demostrar que B > 0; aunque `grind` lo puede hacer también
+  have hB₁ : B ≠ 0 := by
+    intro hB
+    have h₁ : L - 1 = 0 := by
+      apply abs_eq_zero.mp
+      apply le_antisymm
+      · rw [← hB]
+        exact le_max_left |L - 1| |L + 1|
+      · exact abs_nonneg (L - 1)
+    have h₂ : L + 1 = 0 := by
+      apply abs_eq_zero.mp
+      apply le_antisymm
+      · rw [← hB]
+        exact le_max_right |L - 1| |L + 1|
+      · exact abs_nonneg (L + 1)
+    linarith
   have hB₂ : ∀ n ≥ N₁, |b n| ≤ B :=
     fun (n : ℕ) (hn : n ≥ N₁) => abs_sub_le_of_abs_le_max (hN₁ n hn)
+  -- cota superior B para |bₙ|, positiva porque |L+1| y |L-1| alguno es >0
   let ⟨N₂, hN₂⟩ := ha (ε / (2 * B)) (by positivity)
+  -- la cota superior para |aₙ| es ε / (2*B)
   use max N₁ N₂
   intro n hn
   have hn₁ : n ≥ N₁ := trans hn (le_max_left N₁ N₂)
@@ -83,6 +100,33 @@ theorem Reto18 {a b : ℕ → ℝ} {L M : ℝ}
   -- ídem si M = 0, tenemos así que |L|, |M| > 0
   have hL : |L| > 0 := by positivity
   have hM : |M| > 0 := by positivity
-  sorry
+  intro ε₀ hε₀
+  let ε := min ε₀ (4 * |L| * |M|)
+  have hε : ε > 0 := by positivity
+  let ε₁ := ε/(4*|M|)
+  let ε₂ := ε/(4*|L|)
+  let ⟨N₁, hN₁⟩ := ha ε₁ (by positivity)
+  let ⟨N₂, hN₂⟩ := hb ε₂ (by positivity)
+  use max N₁ N₂
+  intro n hn
+  have hnN₁ : n ≥ N₁ := trans hn (le_max_left N₁ N₂)
+  have hnN₂ : n ≥ N₂ := trans hn (le_max_right N₁ N₂)
+  calc
+    _ = |a n * b n - L * M| := rfl
+    _ = |(a n - L) * (b n - M) + (M*(a n - L) + L*(b n - M))| := by ring_nf
+    _ ≤ |(a n - L) * (b n - M)| + |M * (a n - L) + L * (b n - M)| := by
+      rel [abs_add_le ((a n - L) * (b n - M)) (M * (a n - L) + L * (b n - M))]
+    _ ≤ |(a n - L) * (b n - M)| + |M * (a n - L)| + |L * (b n - M)| := by
+      rw [add_assoc, add_le_add_iff_left |(a n - L) * (b n - M)|]
+      rel [abs_add_le (M * (a n - L)) (L * (b n - M))]
+    _ = |a n - L| * |b n - M| + |M| * |a n - L| + |L| * |b n - M| := by
+      rw [abs_mul, abs_mul, abs_mul]
+    _ < ε₁ * ε₂ + |M| * ε₁ + |L| * ε₂ := by rel [hN₁ n hnN₁, hN₂ n hnN₂]
+    _ = ε * ε / (16 * |L| * |M|) + ε / 2 := by unfold ε₁ ε₂; field
+    _ ≤ ε * (4 * |L| * |M|) / (16 * |L| * |M|) + ε / 2 := by
+      rel [min_le_right ε₀ (4 * |L| * |M|)]
+    _ = ε / 4 + ε / 2 := by field
+    _ < ε := by linarith
+    _ ≤ ε₀ := min_le_left ε₀ (4 * |L| * |M|)
 
 #check Reto18
